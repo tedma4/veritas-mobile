9e8b289f92d4fb7bd5d340986bcbd80a2565abef
Vinicio Del Toro <vdeltoropaz@revspringinc.com>
2017-01-22 17:08:46 -0700

    refactoring reply code

---
+++ app/models/post-to-send.ts
@@ -0,0 +1,52 @@
+import { Post } from "../models/post";
+import { User } from "../models/user";
+
+interface PostToSendJSON{
+  originPost: Post,
+  newPost: Post,
+	userList: Array<User>
+}
+
+export class PostToSend{
+  private _originPost: Post;
+  private _newPost: Post;
+	private _userList: Array<User>;
+
+  constructor(object: any){
+		if(object.originPost){
+			this._originPost = new Post(object.originPost);	
+		}else{
+			this._originPost = new Post(object._originPost);
+		}
+		if(object.newPost){
+			this._newPost = new Post(object.newPost);	
+		}else{
+			this._newPost = new Post(object._newPost);
+		}
+	  this._userList = object && object.userList || object && object._userList || undefined;
+  }
+
+	public get originPost(): Post {
+		return this._originPost;
+	}
+
+	public set originPost(value: Post) {
+		this._originPost = value;
+	}
+
+	public get newPost(): Post {
+		return this._newPost;
+	}
+
+	public set newPost(value: Post) {
+		this._newPost = value;
+	}
+
+	public get userList(): Array<User> {
+		return this._userList;
+	}
+
+	public set userList(value: Array<User>) {
+		this._userList = value;
+	}
+}
